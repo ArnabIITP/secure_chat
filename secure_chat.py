57212f46a52df434b458287187070947cc963eb8
ArnabIITP <arnabdasdgp020@gmail.com>
2025-02-24 19:20:57 +0530

    Update secure_chat.py

--- secure_chat.py
+++ secure_chat.py
@@ -187,3 +187,6 @@ tk.Button(button_frame, text="File", command=send_file, bg="#3498db", fg="white"
 
 login_screen()
 root.mainloop()
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=10000, debug=False)
